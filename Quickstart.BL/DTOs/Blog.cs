5635527b285aeb2e54d99b2c3d8533d481a06d00
David Santafe <santafedavid01@gmail.com>
2021-08-07 11:55:43 -0500

    Agregar archivos de proyecto.

---
+++ Quickstart.BL/DTOs/Blog.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quickstart.BL.DTOs
+{
+    public class Blog
+    {
+        [Required(ErrorMessage = "El campo Id es requerido")]
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+    }
+}
