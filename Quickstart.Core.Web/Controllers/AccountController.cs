6d358ab60584d3aba62e8747e648fd07c41b5129
David Santafe <santafedavid01@gmail.com>
2022-02-12 10:54:55 -0500

    2022-02-12 - Api Controller Account y jQuery DataTable

---
+++ Quickstart.Core.Web/Controllers/AccountController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Quickstart.Core.BL.DTOs;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Quickstart.Core.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        #region Properties
+        public class LoginDTO
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+
+        public class RegisterDTO
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]            
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        #endregion
+
+        public AccountController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return Ok(new ResponseDTO
+                    {
+                        Code = (int)HttpStatusCode.BadRequest,
+                        Message = string.Join(",", ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage))
+                    });
+
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                    model.Password,
+                    model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded) return Ok(new ResponseDTO { Code = (int)HttpStatusCode.OK });
+
+                return Ok(new ResponseDTO
+                {
+                    Code = (int)HttpStatusCode.Unauthorized,
+                    Message = "Invalid login attempt."
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ResponseDTO
+                {
+                    Code = (int)HttpStatusCode.InternalServerError,
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegisterDTO model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return Ok(new ResponseDTO
+                    {
+                        Code = (int)HttpStatusCode.BadRequest,
+                        Message = string.Join(",", ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage))
+                    });
+
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) return Ok(new ResponseDTO { Code = (int)HttpStatusCode.OK });
+                else
+                {
+                    foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
+
+                    return Ok(new ResponseDTO
+                    {
+                        Code = (int)HttpStatusCode.BadRequest,
+                        Message = string.Join(",", ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage))
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ResponseDTO
+                {
+                    Code = (int)HttpStatusCode.InternalServerError,
+                    Message = ex.Message
+                });
+            }
+        }
+    }
+}
--- Quickstart.Core.Web/Controllers/BlogsController.cs
+++ Quickstart.Core.Web/Controllers/BlogsController.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
+using Quickstart.Core.BL.DTOs;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Quickstart.Core.Web.Controllers
 {
     public class BlogsController : Controller
     {
+        public static List<BlogDTO> Blogs { get; set; } = new List<BlogDTO> {
+            new BlogDTO { Id = 1, Name = "Azure" },
+            new BlogDTO { Id = 2, Name = "AWS" },
+            new BlogDTO { Id = 3, Name = "GCP" },
+        };
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult IndexJson()
+        {
+            return Json(Blogs);
         }
     }
 }
